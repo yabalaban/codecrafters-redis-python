2d8209673c3c343608b56d5604ae64821ed38b3a
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-10-01 21:29:43 +0100

    pong cmd

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 import socket  # noqa: F401
 
 
+class Command:
+    def send(self, socket):
+        pass
+
+
+class PongCommand(Command):
+    def send(self, socket):
+        socket.send(b'+PONG\r\n')
+
+
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    (sock, addr) = server_socket.accept() # wait for client
+    with sock:
+        pong = PongCommand()
+        pong.send(sock)
+    
 
 if __name__ == "__main__":
     main()
