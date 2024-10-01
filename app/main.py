bad3a5a76212f1910be4300122db9af63ddd06c7
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-10-01 21:31:25 +0100

    recv

--- app/main.py
+++ app/main.py
@@ -15,8 +15,10 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     (sock, addr) = server_socket.accept() # wait for client
     with sock:
-        pong = PongCommand()
-        pong.send(sock)
+        while True: 
+            data = sock.recv(512)
+            pong = PongCommand()
+            pong.send(sock)
     
 
 if __name__ == "__main__":
