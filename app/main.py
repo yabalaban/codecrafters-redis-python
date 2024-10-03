2f13264bf17d19bfd827b636abc467302e33a64e
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-10-03 22:03:03 +0100

    handshake

--- app/main.py
+++ app/main.py
@@ -197,8 +197,6 @@ def psync_cmd(dt: RespArray, writer: asyncio.StreamWriter):
     # replication_id = dt.items[1].value 
     # replication_offset = dt.items[2].value
     writer.write(RespString(f'FULLRESYNC {STATE.replication.master_replid} {STATE.replication.master_repl_offset}'))
-    STATE.db.
-    writer.write(RespString(f'FULLRESYNC {STATE.replication.master_replid} {STATE.replication.master_repl_offset}'))
     
 
 cmds = {
